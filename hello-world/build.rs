f0d82f2f1a79c0d97e0b19cab07299ef71f3b9ed
becominginsane <francesco@zondax.ch>
2022-12-22 10:13:15 +0800

    deps: force cmake release profile

--- hello-world/build.rs
+++ hello-world/build.rs
@@ -5,9 +5,11 @@ fn main() {
     let out = cmake::Config::new(".")
         .init_cxx_cfg(cxx)
         .build_target("hello_world")
+        .profile("RelWithDebInfo")
         .define("CXXBRIDGE_OUT", format!("{}/cxxbridge/sources", out_dir))
         .build();
 
     println!("cargo:rustc-link-search=native={}/build", out.display());
     println!("cargo:rustc-link-lib=static=hello_world");
+    println!("cargo:rerun-if-changed=CMakeLists.txt");
 }
