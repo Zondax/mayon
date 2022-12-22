9a91ff4d363046c8db05851ede3c5dc153303f67
becominginsane <francesco@zondax.ch>
2022-12-22 09:35:04 +0800

    deps(libp2p): add soralog

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -2,3 +2,8 @@ hunter_config(libp2p
     URL https://github.com/libp2p/cpp-libp2p/archive/refs/tags/v0.1.8.zip
     SHA1 9755d12f8f57689eb40e3664266f4b01c7379c27
 )
+
+hunter_config(soralog
+    URL  https://github.com/soramitsu/soralog/archive/refs/tags/v0.1.5.tar.gz
+    SHA1 88664218fc6430ed8d782aaeec665b43ec10c702
+)
