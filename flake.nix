ee32251409ee7265fd0b38750a0b336cc3686a9f
becominginsane <francesco@zondax.ch>
2023-01-25 01:53:04 +0800

    nix(flake): move to flake-parts

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Nix Utils
     nixpkgs.url = "nixpkgs/nixos-22.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -19,33 +19,34 @@
       url = "github:ipetkov/crane";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
         flake-compat.follows = "flake-compat";
       };
     };
   };
 
-  outputs = inputs@{ nixpkgs, flake-utils, fenix, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        rust = fenix.packages.${system}.stable.toolchain;
-        crane = inputs.crane.lib.${system}.overrideToolchain rust;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { inputs', pkgs, ... }:
+        let
+          rust = inputs'.fenix.packages.stable.toolchain;
+          crane = (inputs.crane.mkLib pkgs).overrideToolchain rust;
 
-        deps = pkgs.callPackage ./nix/deps.nix { };
-        tools = pkgs.callPackage ./nix/tools.nix { inherit crane; };
+          deps = pkgs.callPackage ./nix/deps.nix { };
+          tools = pkgs.callPackage ./nix/tools.nix { inherit crane; };
 
-        workspace = pkgs.callPackage crane.buildPackage {
-          stdenv = pkgs.llvmPackages_14.stdenv;
+          workspace = crane.buildPackage {
+            src = ./.;
 
-          src = ./.;
-          nativeBuildInputs = [ pkgs.pkg-config pkgs.cmake ];
-          buildInputs = [ deps.cpp-libp2p ];
+            nativeBuildInputs = [ pkgs.pkg-config pkgs.cmake ];
+            buildInputs = [ deps.cpp-libp2p ];
+          };
+        in {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ workspace deps.cpp-libp2p ];
+            packages = [ pkgs.just rust tools.ffizer pkgs.niv ];
+          };
         };
-      in {
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ workspace deps.cpp-libp2p ];
-          packages = [ pkgs.just rust tools.ffizer pkgs.niv ];
-        };
-      });
+    };
+
 }
