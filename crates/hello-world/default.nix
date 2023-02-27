7ed87c94c771cf30a56c3ab0de710799ea006ff4
becominginsane <francesco@zondax.ch>
2023-02-28 00:28:55 +0800

    feat(nix:cxxbridge): drv to recreate symlinks
    fix(nix): pass CXXBRIDGE_OUT to cmake based on cxxbridge derivation

--- crates/hello-world/default.nix
+++ crates/hello-world/default.nix
@@ -2,11 +2,19 @@
 , cxxbridge-out
 , cpp-libp2p ? (import ../../nix/deps.nix { inherit pkgs; }).cpp-libp2p
 }:
+let
   pname = "hello-world";
+  src = ./.;
+  cxxbridge = import ../cxxbridge.nix { inherit pkgs;
+                                        src = cxxbridge-out;
+                                        crate-src = src;
+                                        crate-name = pname; };
+in
 pkgs.stdenv.mkDerivation {
+  inherit pname src;
   version = "0.1.0";
 
-  src = ./.;
+  cmakeFlags = [ "-DCXXBRIDGE_OUT=${cxxbridge}" ];
 
   nativeBuildInputs = [ pkgs.pkg-config pkgs.cmake cpp-libp2p ];
   buildInputs = [ cpp-libp2p ];
