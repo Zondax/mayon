82ec526d50016cfa1b1f2d8ef8c6b17f943e5fc9
becominginsane <francesco@zondax.ch>
2023-02-28 00:17:50 +0800

    fix(cmake): emulate more cxxbridge initialization
    fix(build): pass cxxbridge root dir instead of sources directly

--- crates/hello-world/build.rs
+++ crates/hello-world/build.rs
@@ -1,22 +1,25 @@
 use nix_build as nix;
 
 fn main() {
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR set during compilation");
+    let cxxbridge_out = format!("{out_dir}/cxxbridge");
+
     let cxx = cxx_build::bridges(vec!["src/lib.rs", "src/foo/mod.rs"]);
 
     let out = if nix::is_nix_available().is_some() {
         nix::Config::new(".").build().expect("nix build ok")
     } else {
+        println!("cargo:rerun-if-changed=CMakeLists.txt");
         cmake::Config::new(".")
             .init_cxx_cfg(cxx)
             .build_target("hello_world")
             .profile("RelWithDebInfo")
+            .define("CXXBRIDGE_OUT", &cxxbridge_out)
             .build()
     };
 
-    println!("cargo:rustc-link-search=native={}/build", out.display());
+    println!("cargo:rustc-link-search=native={}/build/lib", out.display());
     println!("cargo:rustc-link-lib=static=hello_world");
-    println!("cargo:rerun-if-changed=CMakeLists.txt");
-    println!("cargo:rerun-if-changed=default.nix");
     println!("cargo:rerun-if-changed=include/");
     println!("cargo:rerun-if-changed=src/");
 }
