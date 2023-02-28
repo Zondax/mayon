15cb7c5b29beb69594e581a563fcaea4199ae941
becominginsane <francesco@zondax.ch>
2023-02-28 19:30:54 +0800

    feat(poc): nixify build
    feat(cmake): FetchContent asio
    fix(nix:cxxbridge): copy rust/cxx.h to $out
    fix(nix:tree): get path of symlinks along with files

--- crates/asio-poc/build.rs
+++ crates/asio-poc/build.rs
@@ -1,19 +1,27 @@
 fn main() {
     let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR set during compilation");
+    let cxxbridge_out = format!("{out_dir}/cxxbridge");
+
     let mut cxx = cxx_build::bridges(vec!["src/timer.rs", "src/channel.rs", "src/future_void.rs"]);
-    cxx.flag_if_supported("-std=c++20");
 
-    let out = cmake::Config::new(".")
-        .init_cxx_cfg(cxx)
-        .build_target("cpp_asio_poc")
-        .profile("RelWithDebInfo")
-        .define("CXXBRIDGE_OUT", format!("{}/cxxbridge/sources", out_dir))
-        .build();
+    let out = if nix_build::is_nix_available().is_some() {
+        nix_build::Config::new(".")
+            .add_expr("cxxbridge-out", &cxxbridge_out)
+            .build()
+            .expect("nix build ok")
+    } else {
+        println!("cargo:rerun-if-changed=CMakeLists.txt");
+        cmake::Config::new(".")
+            .init_cxx_cfg(cxx)
+            .build_target("cpp_asio_poc")
+            .profile("RelWithDebInfo")
+            .define("CXXBRIDGE_OUT", &cxxbridge_out)
+            .define("HUNTER_ENABLED", "ON")
+            .build()
+    };
 
-    println!("cargo:rustc-link-search=native={}/build", out.display());
+    println!("cargo:rustc-link-search=native={}/build/lib", out.display());
     println!("cargo:rustc-link-lib=static=cpp_asio_poc");
-
-    println!("cargo:rerun-if-changed=CMakeLists.txt");
     println!("cargo:rerun-if-changed=include/");
     println!("cargo:rerun-if-changed=src/");
 }
